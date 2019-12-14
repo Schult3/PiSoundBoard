864e389e5c904ef01db82b8f00ed9fba89fe02e6
Schult3 <damm.schulte@gmail.com>
2019-12-14 10:33:19 +0100

    Commit

---
+++ killswitch.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import os
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+KILLSWITCH = 18
+GPIO.setup(KILLSWITCH, GPIO.IN)
+
+def killswitch():
+	if GPIO.input(KILLSWITCH) != GPIO.HIGH:
+		print("shutdown")
+		os.system("sudo shutdown -h now")
+
+def main():
+	while True:
+		killswitch()
+
+if __name__ == "__main__":
+	main()
--- work.py
+++ work.py
@@ -1,17 +1,25 @@
 import RPi.GPIO as GPIO
 import time
+from shutil import copyfile
+import os
 from soundplayer import SoundPlayer
 
 
-switches = {25: {"filename": "01.mp3"}, 24: {"filename": "02.mp3"}, 23: {"filename": "03.mp3"} }
+switches = {25: {"filename": "01.mp3"}, 24: {"filename": "02.mp3"}, 23: {"filename": "03.mp3"}, 4: {"filename": "04.mp3"}, 17: {"filename": "05.mp3"}, 27: {"filename": "06.mp3"} }
 
-SoundFilePath = "/media/PiSoundBoard/"
+SoundFilePath = "/home/pi/soundfiles/"
 lastPushedSwitch = False
 player = False
 
 def initSwitches():
 	for i in switches:
 		GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+def copyMediaFiles():
+	if len(os.listdir('/media/PiSoundBoard')) > 0:
+		os.system("sudo rm /home/pi/soundfiles/*")
+		os.system("sudo cp -R /media/PiSoundBoard/*.mp3 /home/pi/soundfiles")
 
 
 def readSwitches():
@@ -25,6 +33,7 @@ def readSwitches():
 
 GPIO.setmode(GPIO.BCM)
 initSwitches()
+copyMediaFiles()
 
 if __name__ == '__main__':
 	while True:
@@ -32,6 +41,6 @@ if __name__ == '__main__':
 			lastPushedSwitch = False
 
 		filename = readSwitches()
-		if filename != False:
+		if filename != False and os.path.isfile(SoundFilePath + filename):
 			player = SoundPlayer(SoundFilePath + filename, 0)
 			player.play(1.0)
